c9a5642fe1ebde120cbe68f4ede9f909026a3238
Kolton Koester <koltonkoester10@gmail.com>
2022-09-14 12:21:38 -0400

    Made the character sheet class, added properties related to a DnD character sheet , and started to write out methods

---
+++ DnD-CharacterSheet/DnD-CharacterSheet/CharacterSheet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_CharacterSheet
+{
+    public class CharacterSheet
+    {
+        public string Name { get; set; }
+        public int ArmorClass { get; set; }
+        public Dictionary<String,int> Inventory{ get; set; }
+        public int HitPoints { get; set; }
+        public int Speed { get; set; }
+
+        public CharacterSheet()
+        {
+
+        }
+
+        public void AddItemToInventory()
+        {
+            Inventory.Add(Console.ReadLine(), int.Parse(Console.ReadLine()));
+        }
+        public void RemoveItemFromInventory()
+        {
+            
+        }
+        public void DecreaseArmorClass()
+        {
+
+        }
+        public void IncreaseArmorClass()
+        {
+
+        }
+        public void AttackBot()
+        {
+          
+        }
+    }
+}
